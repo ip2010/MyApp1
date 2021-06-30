950aa8bcff6d69f3e1e451d8c0f1ee3c1776c6c9
Anatoliy <ip2010@yandex.ru>
2021-06-30 13:52:13 +0300

    Добавлена функция для кнопки Count

--- java/com/example/myapp1/MainActivity.kt
+++ java/com/example/myapp1/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,14 @@ class MainActivity : AppCompatActivity() {
     fun toastMe(view: View){
         val myToast = Toast.makeText(this,"Heloo Tolya!!", Toast.LENGTH_SHORT)
         myToast.show()
+    }
+
+    fun countMe(view: View){
+        val countString = textView1.text.toString()
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        textView1.text = count.toString()
+
     }
 }
